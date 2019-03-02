676682076060e3e7a49511779e697189e0e33179
Masetty Abhishek <masetty.abhishek@freshworks.com>
2019-03-02 16:22:19 +0530

    Created Message Model and migration file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-	validates :username, presence: true, length: {minimum: 3, maximum: 20}
-	has_secure_password
+	validates :username, presence: true, length: {minimum: 3, maximum: 20}, uniqueness: {case_sensitive: false}
+ 	has_many :messages
+ 	has_secure_password
 end
